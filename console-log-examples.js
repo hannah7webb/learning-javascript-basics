ed27cfebb7d754aaa5d9e053d8a54036b86fd19a
hannah7webb <hannah.noelle@icloud.com>
2025-07-29 14:16:13 -0700

    first commit

---
+++ console-log-examples.js
@@ -0,0 +1,18 @@
+console.log("Hello, world!"); // prints text to terminal (return type is undefined)
+
+let result = console.log("Hello, again!"); // prints text to terminal, assigns result undefined return type
+console.log(result); // prints result (undefined)
+
+function logger(val) {
+    console.log(val);
+    return val; 
+}
+let text = "hey!"; 
+logger(text); // calls function that prints text to terminal (return type is the val)
+
+let otherText = logger("bye!"); // prints text to terminal, assigns val as return type 
+logger(otherText); // prints otherText, val return type 
+
+
+
+
